7152e9924005743bbcebc437ad79ab81e077b63f
SalmaMej <salma.mejdouba@gmail.com>
2021-01-09 14:57:39 +0100

    2nd commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(date =(req,res,next)=>{
     let newdate=new Date()
     let hours=newdate.getHours()
     let day=newdate.getDay()
-    if((hours<9 && hours>17) || ((day==6 || day==0))){
+    if((hours<9 || hours>17) || ((day==6 || day==0))){
         res.send('<h1><center>We are closed</center></h1><p><center>We work from MONDAY to FRIDAY, from 9am to 5pm</center></p>')
     } else {
         next()
